2c5b08d879e4a125f159ba01731ccb0be9adfa16
wangxb812 <1006089021@qq.com>
2020-12-15 19:55:04 +0800

    剑指offer C++

---
+++ 剑指offer/JZ30.cpp
@@ -0,0 +1,17 @@
+//输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。要求时间复杂度为 O(n).
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int FindGreatestSumOfSubArray(vector<int> array) {
+        int len = array.size();
+        if(len == 0) return 0;
+        int sum = 0,res =0x80000000;
+        for(int i=0;i<len;i++){
+            if(sum<=0)  sum=array[i];
+            else    sum+=array[i];
+            if(sum>res) res = sum;
+        }
+        return res;
+    }
+};
