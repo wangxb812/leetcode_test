e430e8342090c7249df21941f84909dd55b2e2c4
wangxb812 <1006089021@qq.com>
2021-07-22 00:36:01 +0800

    C++

---
+++ 程序员代码面试指南/CD5.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Author: m1ng
+ * @Date: 2021-07-21 00:55:00
+ * @LastEditors: m1ng
+ * @LastEditTime: 2021-07-22 00:35:42
+ * @FilePath: \leetcode_test\程序员代码面试指南\CD5.cpp
+ * @Description: 实现一个特殊功能的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
+ */
+#include<stack>
+using namespace std;
+class solution
+{
+private:
+    stack<int> stacknum;
+    stack<int>  stackmin;
+public:
+    void push(int num){
+        if(stackmin.empty())    stackmin.push(num);
+        else if(stackmin.top()>=num)    stackmin.push(num);
+        stacknum.push(num);
+    }
+    void pop(){
+        if(stacknum.empty())    return;
+        int val=stacknum.top();
+        stacknum.pop();
+        if(val == getmin()) stackmin.pop();
+        return ;
+    }
+    int getmin()
+    {
+        if(stackmin.empty())    return;
+        return stackmin.top();
+    }
+};
+//第一种方法，只压入一次；第二种方法，重复压入最小值，两个栈大小一致。
