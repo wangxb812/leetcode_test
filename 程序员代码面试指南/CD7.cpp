253cf1093c391ddf7933b6358df435aeea820ca2
wangxb812 <1006089021@qq.com>
2021-07-26 11:24:55 +0800

    C++

---
+++ 程序员代码面试指南/CD7.cpp
@@ -0,0 +1,56 @@
+/*@ -2,7 +2,53 @@
+ * @Author: m1ng
+ * @Date: 2021-07-22 00:39:46
+ * @LastEditors: m1ng
+ * @LastEditTime: 2021-07-22 00:39:46
+ * @FilePath: \leetcode_test\CD7.cpp
+ * @LastEditTime: 2021-07-23 01:33:09
+ * @FilePath: \leetcode_test\程序员代码面试指南\CD7.cpp
+ * @Description: header
+ */
+#include<stack>
+#include<iostream>
+using namespace std;
+class Solution
+{
+public:
+//返回栈底元素
+    int getEndRemove(stack<int> st){
+        int res=st.top();
+        st.pop();
+        if(st.empty())  return res;
+        else{
+            int last = getEndRemove(st);
+            st.push(res);
+            return last;
+        }
+    }
+    void reverseStack(stack<int> st)
+    {
+        if(st.empty())  return;
+        int i = getEndRemove(st);
+        reverseStack(st);
+        st.push(i);
+    }
+    Solution(){
+        ;
+    }
+};
+static Solution obj;
+int main(){
+    int n=0;
+    int s=0;
+    stack<int> st;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        st.push(s);
+    }
+    obj.reverseStack(st);
+    for(int i=0;i<st.size();i++)
+    {
+        printf("%n",st.top());
+        st.pop();
+    }
+    return 0;
+}
