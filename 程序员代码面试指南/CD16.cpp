0690f619b203c9a7c3dedac8293e592cf3b2e172
wangxb812 <1006089021@qq.com>
2021-07-29 01:30:44 +0800

    Create CD16.cpp

---
+++ 程序员代码面试指南/CD16.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: m1ng
+ * @Date: 2021-07-29 00:13:15
+ * @LastEditors: m1ng
+ * @LastEditTime: 2021-07-29 00:31:37
+ * @FilePath: \leetcode_test\程序员代码面试指南\CD16.cpp
+ * @Description: header
+ */
+#include <stack>
+#include <iostream>
+using namespace std;
+int getOneNumber()
+{
+
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+}
