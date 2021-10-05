ddf2eb4e65f927a2e40424acaf3f5028a9db82a2
wangxb812 <1006089021@qq.com>
2021-10-05 23:22:31 +0800

    Create CD161.cpp

---
+++ 程序员代码面试指南/CD161.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+# include <vector>
+# include <iostream>
+using namespace std;
+int main()
+{
+    int n,root;
+    cin>>n>>root;
+    vector<vector<int>> nums;
+    for(int i = 0;i<n;i++)
+    {
+        cin>>vector[i][0]>>vector[i][1]>>vector[i][2];
+    }
+    
+    return 0;
+}
